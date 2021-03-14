a724d3257f4e6b5428a20521a43cb0c9cd0268b0
Majed <majed-dalain@hotmail.com>
2021-03-14 11:38:48 +0100

    performance and structure enhancement

--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -1,27 +1,35 @@
-import React from 'react';
+import React , {Component} from 'react';
 import OrderSummary from '../../../components/Burger/OrderSummary/OrderSummary';
 import Aux from '../../../containers/hoc/Aux/Aux';
 import classes from './Modal.module.css';
 import BackDrop from '../BackDrop/BackDrop';
 import Button from '../../UI/Button/Button';
 
-const Modal = (props) => {
-    const modalClasses = props.showModal ? classes.Open : classes.Close;
-    return(
-        <Aux>
-            {props.showModal && <BackDrop/>}
-            <div className={[classes.Modal, modalClasses].join(" ")}>
-                <OrderSummary totalPrice={props.totalPrice} ingridents={props.ingridents}/>
-                <div>
-                    <Button type="Confirm">Confirm</Button>
-                    <Button type = "Cancel" cancel={props.cancel}>Cancel</Button>
+class Modal extends Component  {
+
+    shouldComponentUpdate(nextProps,nextState) {
+        return nextProps.showModal !== this.props.showModal;
+    }
+    
+    render() {
+        const modalClasses = this.props.showModal ? classes.Open : classes.Close;
+        return(
+            <Aux>
+                {this.props.showModal && <BackDrop/>}
+                <div className={[classes.Modal, modalClasses].join(" ")}>
+                    <OrderSummary totalPrice={this.props.totalPrice} ingridents={this.props.ingridents}/>
+                    <div>
+                        <Button type="Confirm">Confirm</Button>
+                        <Button type = "Cancel" cancel={this.props.cancel}>Cancel</Button>
+                    </div>
+                  
                 </div>
-              
-            </div>
-        
-        </Aux>
-        
-    )
+            
+            </Aux>
+            
+        )
+    }
+  
 }
 
 export default Modal; 
